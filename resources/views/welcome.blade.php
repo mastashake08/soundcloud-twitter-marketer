bb72f42059930009016c9e5f6c9be0aa0f2c61b1
Jyrone Parker <jyrone.parker@gmail.com>
2016-10-15 01:57:30 +0000

    visuals

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -68,22 +68,20 @@
         <div class="flex-center position-ref full-height">
             @if (Route::has('login'))
                 <div class="top-right links">
-                    <a href="{{ url('/login') }}">Login</a>
-                    <a href="{{ url('/register') }}">Register</a>
+                    <a href="{{ url('/login') }}">Login With SoundCloud</a>
                 </div>
             @endif
 
             <div class="content">
                 <div class="title m-b-md">
-                    Laravel
+                    SoundCloud Marketer
                 </div>
 
                 <div class="links">
-                    <a href="https://laravel.com/docs">Documentation</a>
-                    <a href="https://laracasts.com">Laracasts</a>
-                    <a href="https://laravel-news.com">News</a>
-                    <a href="https://forge.laravel.com">Forge</a>
-                    <a href="https://github.com/laravel/laravel">GitHub</a>
+                    <a href="/how-it-works">How It Works</a>
+                    <a href="/pricing">Pricing</a>
+                    <a href="http://www.jyroneparker.com">About The Developer</a>
+                    <a href="/support">Support</a>
                 </div>
             </div>
         </div>
